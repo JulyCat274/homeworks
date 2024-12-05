da863af9300022c0600a6dd29293a4559580ed6b
JulyCat274 <dds@7ya1.ru>
2024-12-05 10:43:17 +0400

    Д/з 2 в модуле 10 v2

--- module_10_2.py
+++ module_10_2.py
@@ -11,19 +11,12 @@ class Knight(Thread):
         enemies = 100
         days = 0
         print(f'{self.name}, на нас напали!')
-        for x in range(enemies):
-            if enemies > 0:
-                sleep(1)
-                days += 1
-                enemies -= self.power
-                if days == 1:
-                    print(f'{self.name} сражается {days} день, осталось {enemies} воинов.')
-                elif 1 < days < 5:
-                    print(f'{self.name} сражается {days} дня, осталось {enemies} воинов.')
-                else:
-                    print(f'{self.name} сражается {days} дней, осталось {enemies} воинов.')
-                if enemies <= 0:
-                    print(f'{self.name} одержал победу спустя {days} дней!')
+        while enemies > 0:
+            sleep(1)
+            days += 1
+            enemies -= self.power
+            print(f'{self.name} сражается {days} день/дня/дней, осталось {enemies} воинов.')
+        print(f'{self.name} одержал победу спустя {days} дней!')
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
